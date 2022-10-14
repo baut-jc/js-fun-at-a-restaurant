c32ebe4c9d979ddf5919bc11c92cc949f52436af
Jocelle Bautista <jcllebautista@gmail.com>
2022-10-14 00:23:14 -0600

    Draft taking order

--- src/order.js
+++ src/order.js
@@ -1,6 +1,12 @@
+function takeOrder(order, deliveryOrders) {
+ if (order.orderType === 'delivery') {
+    return deliveryOrders.push(order)
+ } if (deliveryOrders.length >= 3)
+  console.log(deliveryOrders.splice(3, 1))
+}
 
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
