3d734943064a0ac351bd7ff213ee6314e197e61d
Jocelle Bautista <jcllebautista@gmail.com>
2022-10-15 17:43:39 -0600

    Fix breakfast menu

--- src/restaurant.js
+++ src/restaurant.js
@@ -11,20 +11,25 @@ function createRestaurant(foodPlace) {
 }
 
 function addMenuItem(foodPlace, menuDish) {
-    if(menuDish.type === 'breakfast' && !foodPlace.menus.breakfast.includes(menuDish)) {
-      foodPlace.menus.breakfast.push(menuDish)
+  if(menuDish.type === 'breakfast' && !foodPlace.menus.breakfast.includes(menuDish)) {
+    foodPlace.menus.breakfast.push(menuDish)
       return foodPlace.menus.breakfast
-    } else if (menuDish.type === 'lunch' && !foodPlace.menus.lunch.includes(menuDish)) {
+  } else if (menuDish.type === 'lunch' && !foodPlace.menus.lunch.includes(menuDish)) {
     foodPlace.menus.lunch.push(menuDish)
       return foodPlace.menus.lunch
-    } else if (menuDish.type === 'dinner' && !foodPlace.menus.dinner.includes(menuDish)) {
-      foodPlace.menus.dinner.push(menuDish)
+  } else if (menuDish.type === 'dinner' && !foodPlace.menus.dinner.includes(menuDish)) {
+    foodPlace.menus.dinner.push(menuDish)
       return foodPlace.menus.dinner
-    }
+  }
 }
 
-function removeMenuItem() {
-  
+function removeMenuItem(foodPlace, menuDishName, menuType) {
+  // var menuType = foodPlace.menus
+  // console.log('objects: ', foodPlace.menus)
+  for (var i = 0; i < foodPlace.menus.breakfast.length; i++) {
+    foodPlace.menus.breakfast.splice(0, 1)
+    return `No one is eating our ${menuDishName} - it has been removed from the ${menuType} menu!`
+  }
 }
 
 module.exports = {
