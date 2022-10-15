c9a190c0ea3d7808f994e657225dd1a6cc82c7d2
Jocelle Bautista <jcllebautista@gmail.com>
2022-10-15 08:22:16 -0600

    Complete searching for orders

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,17 @@
-
+function createRestaurant(restau) {
+  var restaurant = {
+    name: restau,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+  return restaurant
+}
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant, 
   // addMenuItem,
   // removeMenuItem
 }
