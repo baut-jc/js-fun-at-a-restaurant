7808d7dd92311d958212554cc55a86debe7b6b00
Jocelle Bautista <jcllebautista@gmail.com>
2022-10-15 12:05:28 -0600

    Add object to lunch

--- src/restaurant.js
+++ src/restaurant.js
@@ -11,13 +11,14 @@ function createRestaurant(foodPlace) {
 }
 
 function addMenuItem(foodPlace, foodName) {
-  console.log(foodPlace.menus.lunch)
-  for(var i = 0; i < foodPlace.menus.lunch.length; i++)
-    if(foodPlace.menus.lunch.length <= foodPlace.menus.lunch[i])
+  // console.log('expected string: ', foodName)
+  // console.log(foodPlace.menus.lunch)
+  // for(var i = 0; i < foodPlace.menus.lunch.length; i++)
+    // if(foodPlace.menus.lunch < foodPlace.menus.lunch.length)
     foodPlace.menus.lunch.push(foodName)
 
-  console.log('length: ', foodPlace.menus.lunch.length)
-  console.log('foodName: ', foodPlace.menus.lunch[i])
+  // console.log('length: ', foodPlace.menus.lunch.length)
+  // console.log('foodName: ', foodPlace.menus.lunch[i])
 }
 
 module.exports = {
