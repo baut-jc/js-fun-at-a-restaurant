753324fe8ee0e5d933fc60c0986a1e4f44c2f085
Jocelle Bautista <jcllebautista@gmail.com>
2022-10-16 10:56:11 -0600

    Draft items to remove

--- src/restaurant.js
+++ src/restaurant.js
@@ -10,7 +10,7 @@ function createRestaurant(foodPlace) {
   return restaurant
 }
 
-function addMenuItem(foodPlace, menuDish) {
+function addMenuItem(foodPlace, menuDish) { //(object,  object: restaurant.menus.*breakfast[i])
   if(menuDish.type === 'breakfast' && !foodPlace.menus.breakfast.includes(menuDish)) {
     foodPlace.menus.breakfast.push(menuDish)
       return foodPlace.menus.breakfast
@@ -24,12 +24,16 @@ function addMenuItem(foodPlace, menuDish) {
 }
 
 function removeMenuItem(foodPlace, menuDishName, menuType) {
-  // var menuType = foodPlace.menus
-  // console.log('objects: ', foodPlace.menus)
+  // for (var i = 0; i < foodPlace.menus.length; i++)
+  console.log('objects: ', foodPlace.menus.menuDish) //objects in Array; access all in object
   for (var i = 0; i < foodPlace.menus.breakfast.length; i++) {
     foodPlace.menus.breakfast.splice(0, 1)
     return `No one is eating our ${menuDishName} - it has been removed from the ${menuType} menu!`
   }
+  //if not an object in array; return Sorry, we don't sell ${}, try adding a new recipe!
+      //access the different menus array, dynamically to get to the array's object's foodName/menuDish
+        //menuType === menuDish.type
+      //declare variable/for loop?
 }
 
 module.exports = {
