8b1491064d0bbe01d3d20295e65b56692dcaaaae
Jocelle Bautista <jcllebautista@gmail.com>
2022-10-17 00:13:48 -0600

    Draft Chef

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,23 @@
 class Chef {
-  constructor() {
+  constructor(name, restaurant) {
+    this.name = name
+    this.restaurant = restaurant
     
   }
+  greetCustomer(customer) {
+    if(customer === 'Amy') {
+      return `Good morning, ${customer}!`
+    } else {
+    return `Hello, ${customer}!`
+    }
+  }
+  checkForFood(menuDishName) {
+    // for (var i = 0; i < menuDishName.menus.length; i++)
+     if (menuDishName.name === menuDishName) {
+      return `Yes, we're serving ${menuDishName} today!`
+     }
+  }
+
 }
 
 module.exports = Chef;
